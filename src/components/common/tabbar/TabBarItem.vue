af55ed97109ccc0257b2ba8c6391d427cebe5261
rexzpeng <1486734071@qq.com>
2020-11-28 19:08:30 +0800

    '初始化项目'

---
+++ src/components/common/tabbar/TabBarItem.vue
@@ -0,0 +1,74 @@
+<template>
+  <!-- 所有的item都展示一个图片一个文字 -->
+  <div class="tab-bar-item"
+       @click="itemClick">
+    <div v-if="!isActive">
+      <slot name="item-icon"></slot>
+    </div>
+    <div v-else>
+      <slot name="item-icon-active"></slot>
+    </div>
+
+    <div :style="activeStyle">
+      <slot name="item-text"> </slot>
+    </div>
+
+    <!-- <i class="iconfont">&#xe6eb;</i>
+    <div>首页</div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TabBarItem',
+  props: {
+    path: String,
+    activeColor: {
+      type: String,
+      default: 'red'
+    }
+  },
+  data () {
+    return {
+      //isActive: false,
+
+    }
+  },
+  computed: {
+    isActive () {
+      //  /home -> item1(/home) = true
+      //  /home -> item1(/category) = false
+      //  /home -> item1(/cart) = false
+      //  /home -> item1(/profile) = false
+      return this.$route.path.indexOf(this.path) !== -1
+    },
+    activeStyle () {
+      return this.isActive ? { color: this.activeColor } : {}
+    }
+  },
+  methods: {
+    itemClick () {
+      this.$router.replace(this.path)
+    }
+  }
+}
+</script>
+
+<style>
+.tab-bar-item {
+  flex: 1;
+  text-align: center;
+  height: 49px;
+  font-size: 14px;
+}
+.tab-bar-item i {
+  width: 24px;
+  height: 24px;
+  margin-top: 3px;
+  vertical-align: middle;
+  margin-bottom: 3px;
+}
+.active {
+  color: red;
+}
+</style>
---
+++ src/components/content/mainTabbar/MainTabBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <tab-bar>
+      <tab-bar-item path="/home">
+        <i slot="item-icon"
+           class="iconfont">&#xe662;</i>
+        <i slot="item-icon-active"
+           class="iconfont">&#xe667;</i>
+        <div slot="item-text">首页</div>
+      </tab-bar-item>
+
+      <tab-bar-item path="/category">
+        <i slot="item-icon"
+           class="iconfont">&#xe661;</i>
+        <i slot="item-icon-active"
+           class="iconfont">&#xe660;</i>
+        <div slot="item-text">分类</div>
+      </tab-bar-item>
+
+      <tab-bar-item path="/cart">
+        <i slot="item-icon"
+           class="iconfont">&#xe664;</i>
+        <i slot="item-icon-active"
+           class="iconfont">&#xe663;</i>
+        <div slot="item-text">购物车</div>
+      </tab-bar-item>
+
+      <tab-bar-item path="/profile">
+        <!-- activeColor="blue" -->
+        <i slot="item-icon"
+           class="iconfont">&#xe665;</i>
+        <i slot="item-icon-active"
+           class="iconfont">&#xe666;</i>
+        <div slot="item-text">我的</div>
+      </tab-bar-item>
+
+    </tab-bar>
+  </div>
+</template>
+
+<script>
+import TabBar from 'components/common/tabbar/TabBar'
+import TabBarItem from 'components/common/tabbar/TabBarItem'
+
+export default {
+  name: 'MainTabBar',
+  components: {
+    TabBar,
+    TabBarItem
+
+  }
+}
+</script>
+
+<style>
+</style>
